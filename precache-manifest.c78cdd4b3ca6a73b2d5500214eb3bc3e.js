43fc3f422c3a1b3dd002294b5ceb59693121c990
Chepkyi Mykola <chepkiyn@gmail.com>
2019-11-15 20:37:28 +0200

    Updates

---
+++ precache-manifest.c78cdd4b3ca6a73b2d5500214eb3bc3e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2fe80a2f9111ad8aecf35d1b152315c",
+    "url": "/TaskManager/index.html"
+  },
+  {
+    "revision": "a9bdd963fb72089e7559",
+    "url": "/TaskManager/static/css/main.110e992a.chunk.css"
+  },
+  {
+    "revision": "25a08e888e4e020fb9e0",
+    "url": "/TaskManager/static/js/2.a2fc248d.chunk.js"
+  },
+  {
+    "revision": "a9bdd963fb72089e7559",
+    "url": "/TaskManager/static/js/main.c9b3cf94.chunk.js"
+  },
+  {
+    "revision": "7ec69aa31cd3e103dcd3",
+    "url": "/TaskManager/static/js/runtime-main.68e577ed.js"
+  },
+  {
+    "revision": "6c498791e52ee77eedea219f291f638d",
+    "url": "/TaskManager/static/media/SFProText-Heavy.6c498791.ttf"
+  },
+  {
+    "revision": "ce45a9a9664ff731a806a1c8e52d86b3",
+    "url": "/TaskManager/static/media/SFProText-Regular.ce45a9a9.ttf"
+  },
+  {
+    "revision": "6abf2b142dad9352d32d13cfd7807c0d",
+    "url": "/TaskManager/static/media/SFProText-Semibold.6abf2b14.ttf"
+  }
+]);
